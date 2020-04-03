8b091ae884abb5879c6bec75f9f8b39b32521018
Boris Vinogradov <no111u3@gmail.com>
2020-04-03 18:19:34 +0300

    Add blinking led to first base app

--- src/first.c
+++ src/first.c
@@ -1,10 +1,18 @@
-#include <avr/io.h>
+#define F_CPU 8000000L
 
-#define XTAL 8000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define XTAL F_CPU
 #define baudrate 19200L
 #define bauddivider (XTAL / (16 * baudrate) - 1)
 #define HI(x) ((x) >> 8)
 #define LO(x) ((x) & 0xff)
+
+#define LED1 4
+#define LED2 5
+#define LED_PORT PORTD
+#define LED_DDR DDRD
 
 void hwinit(void) {
 	/* UART0 initialization */
@@ -13,12 +21,23 @@ void hwinit(void) {
 	UCSR0A = 0;
 	UCSR0B = 1 << RXEN0 | 1 << TXEN0 | 1 << RXCIE0 | 0 << TXCIE0;
 	UCSR0C = 1 << UCSZ00 | 1 << UCSZ01;
+
+	/* GPIO initialization */
+	LED_DDR = 1 << LED1;
 }
 
 int main(void) {
-	uint8_t i;
+	volatile uint8_t i;
 
 	hwinit();
+
+	while (1) {
+		i++;
+		LED_PORT = 0 << LED1;
+		_delay_ms(1000);
+		LED_PORT = 1 << LED1;
+		_delay_ms(1000);
+	}
 
 	return 0;
 }
